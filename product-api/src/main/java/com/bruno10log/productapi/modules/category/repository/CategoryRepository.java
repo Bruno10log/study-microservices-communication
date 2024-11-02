5b267919497e6030c2a51a2fe4d7a4f2195f2ff1
Bruno10log <bmeira.bms@gmail.com>
2024-11-02 16:02:46 +0000

    Category get methods

--- product-api/src/main/java/com/bruno10log/productapi/modules/category/repository/CategoryRepository.java
+++ product-api/src/main/java/com/bruno10log/productapi/modules/category/repository/CategoryRepository.java
@@ -1,7 +1,13 @@
 package com.bruno10log.productapi.modules.category.repository;
 
+import com.bruno10log.productapi.modules.category.dto.CategoryResponse;
 import com.bruno10log.productapi.modules.category.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+    List<CategoryResponse> findByDescriptionIgnoreCaseContaining(String description);
+
 }
--- product-api/src/main/java/com/bruno10log/productapi/modules/category/service/CategoryService.java
+++ product-api/src/main/java/com/bruno10log/productapi/modules/category/service/CategoryService.java
@@ -5,9 +5,11 @@ import com.bruno10log.productapi.modules.category.dto.CategoryRequest;
 import com.bruno10log.productapi.modules.category.dto.CategoryResponse;
 import com.bruno10log.productapi.modules.category.model.Category;
 import com.bruno10log.productapi.modules.category.repository.CategoryRepository;
-import com.bruno10log.productapi.modules.supplier.model.Supplier;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.springframework.util.ObjectUtils.isEmpty;
 
@@ -17,7 +19,28 @@ public class CategoryService {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    public CategoryResponse findByIdResponse(Integer id) {
+        return CategoryResponse.of(findById(id));
+    }
+
+    public List<CategoryResponse> findAll() {
+        return categoryRepository.findAll()
+                                    .stream().map(CategoryResponse::of)
+                                    .collect(Collectors.toList());
+    }
+
+    public List<CategoryResponse> findByDescription(String description) {
+        if(isEmpty(description)) {
+            throw new ValidationException("The category description must be informed");
+        }
+        return categoryRepository.findByDescriptionIgnoreCaseContaining(description);
+    }
+
     public Category findById(Integer id) {
+
+        if(isEmpty(id)) {
+            throw new ValidationException("The category ID was not informed");
+        }
         return categoryRepository.findById(id)
                 .orElseThrow(() -> new ValidationException("There is no category for the given ID."));
     }
